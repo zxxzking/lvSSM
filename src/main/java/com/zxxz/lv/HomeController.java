1b547e5b5ecef825b8a69399921ae596528678ec
zxxzking <992072389@qq.com>
2017-07-10 10:31:55 +0800

    1

--- src/main/java/com/zxxz/lv/HomeController.java
+++ src/main/java/com/zxxz/lv/HomeController.java
@@ -1,6 +1,6 @@
 package com.zxxz.lv;
 
-import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
@@ -24,16 +24,13 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String home(Locale locale, Model model, HttpServletRequest request) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		String date = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss").format(new Date());
 
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
+		model.addAttribute("serverTime", date);
 
 		return "home";
 	}
