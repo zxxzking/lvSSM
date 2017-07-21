6dbe2baa7c33a6a3e236840f73930baf693a608f
吕泉 <lvquan@paicaifu.com>
2017-07-21 10:24:36 +0800

    服务器端获取请求源以解决跨域问题

--- src/main/java/com/zxxz/filter/DomainFilter.java
+++ src/main/java/com/zxxz/filter/DomainFilter.java
@@ -7,7 +7,25 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * <pre>
+ * Description	处理跨域请求过滤器
+ * Copyright:	Copyright (c) 2017  
+ * Company:		
+ * Author:		Lv
+ * Version:		1.0  
+ * Create at:	2017年7月21日 上午9:19:50  
+ *  
+ * 修改历史:
+ * 日期    作者    版本  修改描述
+ * ------------------------------------------------------------------  
+ * 
+ * </pre>
+ */
 public class DomainFilter implements Filter{
 
 	@Override
@@ -20,11 +38,25 @@ public class DomainFilter implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
 			ServletException {
 		HttpServletResponse resp = (HttpServletResponse)response;
+		HttpServletRequest req = (HttpServletRequest)request;
+		// 获取真实ip
+		String ip = req.getHeader("x-forwarded-for");     
+	    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {     
+	         ip = req.getHeader("Proxy-Client-IP");     
+	    }     
+	    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {     
+	         ip = req.getHeader("WL-Proxy-Client-IP");     
+	    }     
+	    if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {     
+	          ip = req.getRemoteAddr();     
+	    }
+	    //获取请求的源
+	    String url = req.getHeader("Origin");
 		resp.setHeader("Access-Control-Allow-Headers", "X-Requested-With, accept, content-type, xxxx");
 		resp.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
-		resp.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8020"); 
+		resp.setHeader("Access-Control-Allow-Origin", url); 
 		resp.setHeader("Access-Control-Allow-Credentials", "true"); 
-		chain.doFilter(request, response);
+		chain.doFilter(request, resp);
 	}
 
 	@Override
